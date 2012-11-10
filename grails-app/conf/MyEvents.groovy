dbcdfe95e27e03bb7aa9975cb2975d4e6b20b814
Clemens <clemens@heypanda.de>
2012-11-10 16:16:46 +0100

    removed comments

--- grails-app/conf/MyEvents.groovy
+++ grails-app/conf/MyEvents.groovy
@@ -1,11 +1,3 @@
-/**
- * Copyright 2012 HearDis Corporate Sound
- */
-
-/**
- * @author Clemens (clemens@heypanda.de)
- *
- */
 events = {    
     'savedTodo' browser:true // allows browser push on this topic
 }
